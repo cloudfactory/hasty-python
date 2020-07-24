0f046b6b145748ba26dab1144621de3d98695ff3
Maxime Ellerbach <maxime@ellerbach.net>
2020-07-24 11:35:25 +0100

    added a setup.py file to install the package with pip

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'hasty-python',
+    version = '1.0',
+    description = 'this is a python package to interface with the hasty API',
+    url = 'https://github.com/hasty-ai/hasty-python/',
+    author = 'Kostya',
+    licence = 'MIT License',
+    packages = ['hasty'],
+    install_requires = ['requests >= 2.23.0']
+)
