3098a987ee54a1366f2175e09bdb2ef4585756df
Maxime Ellerbach <maxime@ellerbach.net>
2020-07-24 11:48:24 +0100

    changed the name of the package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name = 'hasty-python',
+    name = 'hastypython',
     version = '1.0',
     description = 'this is a python package to interface with the hasty API',
     url = 'https://github.com/hasty-ai/hasty-python/',
