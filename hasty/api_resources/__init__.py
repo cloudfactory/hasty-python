4e6c2bf529d90600ed331c0ceca538b51b681246
Maxime Ellerbach <maxime@ellerbach.net>
2020-07-27 14:30:24 +0100

    quick bug fix

--- hasty/api_resources/__init__.py
+++ hasty/api_resources/__init__.py
@@ -1,7 +1,9 @@
 from ..api_resources.dataset import Dataset
 from ..api_resources.image import Image
-from ..api_resources.label import Label, LabelAttribute
-from ..api_resources.label_class import LabelClass, LabelClassAttribute, LabelAttributeLOV
+from ..api_resources.label import Label
+from ..api_resources.label_attribute import LabelAttribute
+from ..api_resources.label_class_attribute import LabelClassAttribute
+from ..api_resources.label_class import LabelClass, LabelAttributeLOV
 from ..api_resources.project import Project
 from ..api_resources.workspace import Workspace
 from ..api_resources.api import API
