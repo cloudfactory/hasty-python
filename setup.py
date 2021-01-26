778c01c2f59fc29c1a85c99d3bfbcc58c99d5047
kostya <proskudin@gmail.com>
2021-01-26 21:47:00 +0100

    Update setup and license

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
+import codecs
+import re
 from setuptools import setup
 
+with codecs.open("minio/__init__.py") as file:
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+        file.read(),
+        re.MULTILINE,
+    ).group(1)
+
 setup(
-    name='hasty_python',
-    version='1.0',
-    description='this is a python package to interface with the hasty API',
+    name='hasty',
+    version=version,
+    description='Python package to interface with the Hasty API',
     url='https://github.com/hasty-ai/hasty-python/',
-    author='Kostya, Maxime Ellerbach',
+    author='Hasty GmbH',
+    author_email="herbert@hasty.ai",
     licence='MIT License',
+    long_description_content_type="text/markdown",
     packages=['hasty'],
     install_requires=['requests >= 2.23.0']
 )
