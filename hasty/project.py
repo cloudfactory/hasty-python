12638b1d91a8d41103ccb39ce4c91e66d576fe20
Igor Tumanov <igor.tumanov1@gmail.com>
2022-04-06 11:38:18 +0200

    - fixed signature

--- hasty/project.py
+++ hasty/project.py
@@ -1,5 +1,5 @@
 from collections import OrderedDict
-from typing import List, Union
+from typing import List, Union, Optional
 
 from .attribute import Attribute
 from .constants import ImageStatus, SemanticFormat, VALID_EXPORT_FORMATS, VALID_SEMANTIC_ORDER, \
@@ -267,8 +267,8 @@ class Project(HastyObject):
         return PaginatedList(Attribute, self._requester,
                              Attribute.endpoint.format(project_id=self._id))
 
-    def create_attribute(self, name: str, attribute_type: str, description: str, norder: float,
-                         values: List[str] = None):
+    def create_attribute(self, name: str, attribute_type: str, description: Optional[str] = None,
+               norder: Optional[float] = None, values: List[str] = None):
         """
         Create attribute, returns :py:class:`~hasty.Attribute` object.
 
